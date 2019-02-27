2bce09d632aec4bcf210f231cb0e0ed9fd5f466e
郑茜 <2543473813@qq.com>
2019-02-27 14:51:53 +0800

    blog test

---
+++ build/precache-manifest.fc95b59f02f7fa4023f87f76e2595ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/index.d41d8cd9.less"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "830627eb1c63589de7ad",
+    "url": "/static/js/main.830627eb.chunk.js"
+  },
+  {
+    "revision": "c2645dbe28b7555d229d",
+    "url": "/static/js/1.c2645dbe.chunk.js"
+  },
+  {
+    "revision": "830627eb1c63589de7ad",
+    "url": "/static/css/main.33650709.chunk.css"
+  },
+  {
+    "revision": "c2645dbe28b7555d229d",
+    "url": "/static/css/1.d7accd2b.chunk.css"
+  },
+  {
+    "revision": "a12adefde5da125c88a13a31cd7d0d14",
+    "url": "/index.html"
+  }
+];
